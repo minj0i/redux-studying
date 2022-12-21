54aa1023a9a479044054874cb237343bcee76008
MinjeongKim <mjkim9139@gmail.com>
2022-12-21 15:50:48 +0800

    subscribe

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -3,9 +3,12 @@ import { legacy_createStore } from 'redux';
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
+number.innerText = 0;
 
+// data modifier
 // count: state
 const countModifier = (count = 0, action) => {
+  console.log(count, action)
   if (action.type === 'ADD') {
     return count + 1
   } else if(action.type === 'MINUS') {
@@ -16,11 +19,18 @@ const countModifier = (count = 0, action) => {
 }
 const countStore = legacy_createStore(countModifier)
 
-countStore.dispatch({ type: "ADD"})
-countStore.dispatch({ type: "ADD"})
-countStore.dispatch({ type: "ADD"})
-countStore.dispatch({ type: "ADD"})
-countStore.dispatch({ type: "ADD"})
-countStore.dispatch({ type: "MINUS"})
+const onChange = () => {
+  number.innerText = countStore.getState()
+}
+countStore.subscribe(onChange)
 
-console.log(countStore.getState())
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" })
+}
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS"})
+}
+
+add.addEventListener("click", handleAdd)
+minus.addEventListener("click", handleMinus)
