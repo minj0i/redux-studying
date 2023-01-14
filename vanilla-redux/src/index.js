c067aec13695e96b8c4cd2e1b84abb00b7647571
MinjeongKim <mjkim9139@gmail.com>
2023-01-15 00:05:23 +0800

    Valina ToDo setup

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,18 +1,32 @@
+import { legacy_createStore } from "redux";
+
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createToDo = (toDo) => {
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  ul.appendChild(li);
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+// NEVER USE MUTATE STATE
+const reducer = (state = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
 };
+
+const store = legacy_createStore(reducer);
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  createToDo(toDo);
+  store.dispatch({ type: ADD_TODO, text: toDo });
 };
 
 form.addEventListener("submit", onSubmit);
