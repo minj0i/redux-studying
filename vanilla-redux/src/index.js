5162cafd39743aa974e9f9ad2cfde1268e97eacc
MinjeongKim <mjkim9139@gmail.com>
2023-01-14 23:57:46 +0800

    Change to To Do list

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,41 +1,18 @@
-import { legacy_createStore } from 'redux';
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-number.innerText = 0;
+const createToDo = (toDo) => {
+  const li = document.createElement("li");
+  li.innerText = toDo;
+  ul.appendChild(li);
+};
 
-const ADD = 'ADD'
-const MINUS = 'MINUS'
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  createToDo(toDo);
+};
 
-// data modifier
-// count: state
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return count + 1
-    case MINUS:
-      return count - 1
-    default: 
-      return count
-  }
-}
-
-const countStore = legacy_createStore(countModifier)
-
-const onChange = () => {
-  number.innerText = countStore.getState()
-}
-countStore.subscribe(onChange)
-
-// action should be a plain object, and must have a type
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD })
-}
-
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS})
-}
-
-add.addEventListener("click", handleAdd)
-minus.addEventListener("click", handleMinus)
+form.addEventListener("submit", onSubmit);
